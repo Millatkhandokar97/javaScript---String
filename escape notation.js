46a0b7ffe887785a13ad4ac84b67cff520343d88
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-06 00:15:08 +0600

    Create escape notation.js

---
+++ escape notation.js
@@ -0,0 +1,13 @@
+//Escape Notation
+
+let str = 'This a javaScript Litiral String'
+let str1 = "This a javaScript Litiral 'String'"
+let str2 = 'This a javaScript Litiral "String"'
+let str3 = 'This a javaScript Litiral \'String\''
+let str4 = 'This a javaScript Litiral \nString'
+
+console.log(str);
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
