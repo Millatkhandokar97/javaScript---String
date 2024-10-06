19c90b6f34f5130f821e42bc49b0ffe8779320d4
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-07 00:53:26 +0600

    string comparison in Javascript

---
+++ string omparison.js
@@ -0,0 +1,15 @@
+//String Comparison
+
+let a = 'abc'
+let b = 'acd'
+
+console.log(a===b);
+console.log(a < b);
+console.log(a > b);
+
+console.log('a' < 'z' && 'x' < 'z');
+
+console.log('001' == 1);
+console.log('002' > 1);
+console.log('z' > '1');
+console.log('z' > 1);
